4f775838b91505e4653cbddeb4c2abe4889f7dab
Moritz Mock <mock.moritz@rolmail.net>
2020-09-10 18:42:42 +0200

    new build

--- dist/index.js
+++ dist/index.js
@@ -23041,14 +23041,12 @@ const findError = (data) => {
 
 
 const find = async () => {
-    const globber = await glob.create('src/*')
+    const globber = await glob.create()
 
     let errorsGlobal = [] //all errors over all files
 
     for await (const filePath of globber.globGenerator()) {
         fs.readFile(filePath, 'utf8', (err, data) => {
-            console.log('------------')
-            console.log(data)
             const changedData = data.toString().replace('"', '').split('\n')
             const errors = findError(changedData)
             if(errors.length > 0) {
